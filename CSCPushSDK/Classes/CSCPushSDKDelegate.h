c1240c9556f10e86f4bd752ce52b68f1356fe816
TerasaLing <1287835607@qq.com>
2023-06-12 14:14:30 +0800

    add register sdk function

--- CSCPushSDK/Classes/CSCPushSDKDelegate.h
+++ CSCPushSDK/Classes/CSCPushSDKDelegate.h
@@ -7,11 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <GTSDK/GeTuiSdk.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CSCPushSDKDelegate : NSObject
--(void)testPrint1;
+
+//注册sdk
++ (void)registerSDKWithAppId:(NSString *)appid appKey:(NSString *)appKey appSecret:(NSString *)appSecret delegate:(id<GeTuiSdkDelegate>)delegate launchingOptions:(NSDictionary * __nullable)launchOptions;
+
+//获取CID
++ (NSString *)getSDKClientId;
+
+//注册远程通知
++ (void)registerRemoteNotification:(UNAuthorizationOptions)types;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- CSCPushSDK/Classes/CSCPushSDKDelegate.m
+++ CSCPushSDK/Classes/CSCPushSDKDelegate.m
@@ -9,7 +9,18 @@
 #import "CSCPushSDKDelegate.h"
 
 @implementation CSCPushSDKDelegate
--(void)testPrint1{
-    NSLog(@"CSC Push SDK pod create successful!!");
+
++ (void)registerSDKWithAppId:(NSString *)appid appKey:(NSString *)appKey appSecret:(NSString *)appSecret delegate:(id<GeTuiSdkDelegate>)delegate launchingOptions:(NSDictionary * __nullable)launchOptions{
+    // 通过个推平台分配的appId、 appKey 、appSecret 启动SDK，注：该方法需要在主线程中调用
+       [GeTuiSdk startSdkWithAppId:appid appKey:appKey appSecret:appSecret delegate:delegate launchingOptions:launchOptions];
 }
+
++ (NSString *)getSDKClientId{
+    return [GeTuiSdk clientId];
+}
+
++ (void)registerRemoteNotification:(UNAuthorizationOptions)types{
+    [GeTuiSdk registerRemoteNotification:types];
+}
+
 @end
